4b225926dda9e4fd658caad19971f472257046e9
Bradford Melluish <bradmelluish@gmail.com>
2016-09-28 19:54:15 -0700

    Controller Refactor
    Implements refactored controllers to avoid issue where request.js event structure was leaving out data chunks. Can also possibly be implemented using .on('end', cb) structure, but this fix is less verbose and fits the requirements of the program.

--- server/resources/user/controller.js
+++ server/resources/user/controller.js
@@ -4,41 +4,51 @@ const url = mw.urls.user;
 
 module.exports = {
   getUser(req, res) {
-    var id = req.params.id;
-    request.get(`${url}/api/user${id ? `/${req.params.id}` : ''}`)
-      .on('error', err => res.status(err.statusCode).send(err))
-      .on('response', resp => resp.on('data', data =>
-        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
-      ));
+    let id = req.params.id;
+    request({
+      method: 'GET',
+      uri: `${url}/api/user${id ? `/${req.params.id}` : ''}`
+    }, (err, resp, body) => err ?
+      res.status(err.statusCode).send(err)
+      : res.status(resp.statusCode).send(JSON.parse(body))
+    );
   },
   signin(req, res) {
-    request.get(`${url}/api/user/${req.params.username}/${req.params.username}`)
-      .on('error', err => res.status(err.statusCode).send(err))
-      .on('response', resp => resp.on('data', data =>
-        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
-      ));
+    request({
+      method: 'GET',
+      uri: `${url}/api/user/${req.params.username}/${req.params.username}`
+    }, (err, resp, body) => err ?
+      res.status(err.statusCode).send(err)
+      : res.status(resp.statusCode).send(JSON.parse(body))
+    );
   },
   createUser(req, res) {
-    request.post(`${url}/api/user/${req.params.username}/${req.params.username}`)
-      .form(req.body)
-      .on('error', err => res.status(err.statusCode).send(err))
-      .on('response', resp => resp.on('data', data =>
-        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
-      ));
+    request({
+      method: 'POST',
+      uri:`${url}/api/user/${req.params.username}/${req.params.username}`,
+      json: req.body
+    }, (err, resp, body) => err ?
+      res.status(err.statusCode).send(err)
+      : res.status(resp.statusCode).send(JSON.parse(body))
+    );
   },
   editUser(req, res) {
-    request.put(`${url}/api/user/${req.params.username}/${req.params.username}`)
-      .form(req.body)
-      .on('error', err => res.status(err.statusCode).send(err))
-      .on('response', resp => resp.on('data', data =>
-        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
-      ));
+    request({
+      method: 'PUT',
+      uri: `${url}/api/user/${req.params.username}/${req.params.username}`,
+      json: req.body
+    }, (err, resp, body) => err ? 
+      res.status(err.statusCode).send(err)
+      : res.status(resp.statusCode).send(JSON.parse(body))
+    );
   },
   deleteUser(req, res) {
-    request.delete(`${url}/api/user/${req.params.username}/${req.params.username}`)
-      .on('error', err => res.status(err.statusCode).send(err))
-      .on('response', resp => resp.on('data', data =>
-        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
-      ));
+    request({
+      method: 'DELETE',
+      uri: `${url}/api/user/${req.params.username}/${req.params.username}`
+    }, (err, resp, body) => err ? 
+      res.status(err.statusCode).send(err)
+      : res.status(resp.statusCode).send(JSON.parse(body))
+    );
   }
 };
