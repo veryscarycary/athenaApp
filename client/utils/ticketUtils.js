c48e9479471c69ff037548695602e17f3e98d92d
Cary <carymeskell@gmail.com>
2016-09-28 22:36:51 -0700

    Fix modal so that it shows selected ticket
    Also make some changes to the ticketUtils to get ready for
    create ticket post to database

--- client/utils/ticketUtils.js
+++ client/utils/ticketUtils.js
@@ -15,6 +15,36 @@ const ticketUtils = {
     .catch(error => {
       console.log(error, 'There was an error getting the tickets!');
     });
+  },
+  submitNewTicket: () => {
+    return fetch('http://localhost:3000/api/ticket', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: document.getElementById('title').value,
+        issuePreview: document.getElementById('issue').value.slice(0, 50),
+        issue: document.getElementById('issue').value,
+        customerId: document.getElementById('customerId').value,
+        product: document.getElementById('product').value,
+        solution: document.getElementById('solution').value,
+        // relatedArticles: document.getElementById('relatedArticles').value,
+        // relatedProducts: document.getElementById('relatedProducts').value,
+        resolved: true,
+        authorId: JSON.stringify(Math.floor(Math.random * 1000)),
+        dateSubmitted: new Date(),
+        datesOpened: [[new Date(), 'Cary']],
+        checkedOut: true
+      })
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error, 'There was an error getting the tickets!');
+    });
   }
 };
 
