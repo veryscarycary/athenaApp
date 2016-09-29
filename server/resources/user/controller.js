3b6b23d013f4374d9afa59559b3698da1801a4ce
Bradford Melluish <bradmelluish@gmail.com>
2016-09-28 17:17:05 -0700

    Controller and Router Refactor
    Improves syntax and cleans up loose ends.

--- server/resources/user/controller.js
+++ server/resources/user/controller.js
@@ -4,39 +4,41 @@ const url = mw.urls.user;
 
 module.exports = {
   getUser(req, res) {
-    request({
-      method: 'GET',
-      uri: `${url}/api/${req.params.username}`
-    }, (err, response, body) => err ?
-      res.status(404).send(err)
-      : res.status(200).send(body)
-    );
+    let id = req.params.id;
+    request.get(`${url}/api/user${id ? `/${req.params.id}` : ''}`)
+      .on('error', err => res.status(err.statusCode).send(err))
+      .on('response', resp => resp.on('data', data =>
+        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
+      ));
   },
   signin(req, res) {
-    request(`${url}/api/${req.params.username}`, (err, response, body) => err ?
-      res.status(404).send(err)
-      : res.status(200).send(body)
-    );
+    request.get(`${url}/api/user/${req.params.username}/${req.params.username}`)
+      .on('error', err => res.status(err.statusCode).send(err))
+      .on('response', resp => resp.on('data', data =>
+        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
+      ));
   },
   createUser(req, res) {
-    request({
-        method: 'POST', 
-        uri: `${url}/api/${req.params.username}`
-      }, (err, response, body) => err ?
-        res.status(404).send(err)
-        : res.status(200).send(body)
-    );
+    request.post(`${url}/api/user/${req.params.username}/${req.params.username}`)
+      .form(req.body)
+      .on('error', err => res.status(err.statusCode).send(err))
+      .on('response', resp => resp.on('data', data =>
+        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
+      ));
   },
   editUser(req, res) {
-    request(`${url}/api/${req.params.username}`, (err, response, body) => err ?
-      res.status(404).send(err)
-      : res.status(200).send(body)
-    );
+    request.put(`${url}/api/user/${req.params.username}/${req.params.username}`)
+      .form(req.body)
+      .on('error', err => res.status(err.statusCode).send(err))
+      .on('response', resp => resp.on('data', data =>
+        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
+      ));
   },
   deleteUser(req, res) {
-    request(`${url}/api/${req.params.username}`, (err, response, body) => err ?
-      res.status(404).send(err)
-      : res.status(200).send(body)
-    );
+    request.delete(`${url}/api/user/${req.params.username}/${req.params.username}`)
+      .on('error', err => res.status(err.statusCode).send(err))
+      .on('response', resp => resp.on('data', data =>
+        res.status(resp.statusCode).send(JSON.stringify(JSON.parse(data)))
+      ));
   }
 };
