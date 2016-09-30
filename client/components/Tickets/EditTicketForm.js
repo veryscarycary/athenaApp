7e22ee686df006b99c1c5b5a83e7a7c11eafbeb9
Cary <carymeskell@gmail.com>
2016-09-30 12:19:26 -0700

    CRUD actions tested and working, editboxes has previews

--- client/components/Tickets/EditTicketForm.js
+++ client/components/Tickets/EditTicketForm.js
@@ -8,44 +8,103 @@ const editTicket = (ticketId) => {
   ticketUtils.editTicket(ticketId);
 };
 
-const EditTicketForm = ({ticket}) => (
-  <div className='row'>
-    <div className='col-xs-12 editTicketForm'>
-      <form action='#' method='' onSubmit={() => {editTicket(ticket._id)}}>
-        <div className='form-group'>
-          <label htmlFor='editTitle'>Title:</label>
-          <input type='text' className='form-control' id='editTitle' value={ticket.title} />
+class EditTicketForm extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      editTitle: this.props.ticket.title,
+      editIssue: this.props.ticket.issue,
+      editProduct: this.props.ticket.product,
+      editCustomerId: this.props.ticket.customerId,
+      editRelatedArticles: this.props.ticket.relatedArticles,
+      editRelatedProducts: this.props.ticket.relatedProducts,
+      editSolution: this.props.ticket.solution
+    }
+  }
+
+  setTitle (event) {
+    this.setState({
+      editTitle: event.target.value,
+    });
+  }
+  setIssue (event) {
+    this.setState({
+      editIssue: event.target.value,
+    });
+  }
+
+  setProduct (event) {
+    this.setState({
+      editProduct: event.target.value,
+    });
+  }
+
+  setCustomerId (event) {
+    this.setState({
+      editCustomerId: event.target.value,
+    });
+  }
+
+  setRelatedArticles (event) {
+    this.setState({
+      editRelatedArticles: event.target.value,
+    });
+  }
+
+  setRelatedArticles (event) {
+    this.setState({
+      editRelatedProducts: event.target.value,
+    });
+  }
+
+  setSolution (event) {
+    this.setState({
+      editSolution: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className='row'>
+        <div className='col-xs-12 editTicketForm'>
+          <form action='#' method='' onSubmit={() => {editTicket(this.props.ticket._id)}}>
+            <div className='form-group'>
+              <label htmlFor='editTitle'>Title:</label>
+              <input type='text' className='form-control' id='editTitle' onChange={this.setTitle} value={this.state.editTitle} />
+            </div>
+            <div className='form-group'>
+            <label htmlFor='editIssue'>Issue:</label>
+            <input type='text' className='form-control' id='editIssue' onChange={this.setIssue} value={this.state.editIssue} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='editProduct'>Product:</label>
+              <input type='text' className='form-control' id='editProduct' onChange={this.setIssue} value={this.state.editProduct} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='editCustomerId'>Customer Id:</label>
+              <input type='text' className='form-control' id='editCustomerId' onChange={this.setIssue} value={this.state.editCustomerId}/>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='editRelatedArticles'>Related Articles:</label>
+              <input type='text' className='form-control' id='editRelatedArticles' onChange={this.setIssue} value={this.state.editRelatedArticles}/>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='editRelatedProducts'>Related Products:</label>
+              <input type='text' className='form-control' id='editRelatedProducts' onChange={this.setIssue} value={this.state.editRelatedProducts}/>
+            </div>
+            <div className='form-group'>
+              <label htmlFor="editSolution">Solution:</label>
+              <textarea className="form-control" rows="5" id="editSolution" onChange={this.setIssue} value={this.state.editSolution}></textarea>
+            </div>
+            <div className='form-group'>
+              <input type='submit' className='btn btn-default' id='editSubmit' />
+            </div>
+          </form>
         </div>
-        <div className='form-group'>
-        <label htmlFor='editIssue'>Issue:</label>
-        <input type='text' className='form-control' id='editIssue' value={ticket.issue} />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='editProduct'>Product:</label>
-          <input type='text' className='form-control' id='editProduct' value={ticket.product} />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='editCustomerId'>Customer Id:</label>
-          <input type='text' className='form-control' id='editCustomerId' value={ticket.customerId}/>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='editRelatedArticles'>Related Articles:</label>
-          <input type='text' className='form-control' id='editRelatedArticles' value={ticket.relatedArticles}/>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='editRelatedProducts'>Related Products:</label>
-          <input type='text' className='form-control' id='editRelatedProducts' value={ticket.relatedProducts}/>
-        </div>
-        <div className='form-group'>
-          <label htmlFor="editSolution">Solution:</label>
-          <textarea className="form-control" rows="5" id="editSolution" value={ticket.solution}></textarea>
-        </div>
-        <div className='form-group'>
-          <input type='submit' className='btn btn-default' id='editSubmit' />
-        </div>
-      </form>
-    </div>
-  </div>
-);
+      </div>
+    )
+  };
+};
 
 export default EditTicketForm;
