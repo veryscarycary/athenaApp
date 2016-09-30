1d2790ff404b458091d498a963c8d6ca9afdfaaa
Cary <carymeskell@gmail.com>
2016-09-29 21:52:25 -0700

    Make ticketinfo text appear in the text edit

--- client/components/Tickets/TicketModal.js
+++ client/components/Tickets/TicketModal.js
@@ -1,48 +1,65 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import ticketActionCreators from '../../actions/index';
 import EditTicketForm from './EditTicketForm';
 
-const ticketModalInfo = function (ticket) {
-  var ticketInfo = [];
 
-  for (var key in ticket) {
-    ticketInfo.push([key, ticket[key]]);
+class TicketModal extends React.Component {
+  constructor (props) {
+    super(props)
+
   }
 
-  return ticketInfo;
-};
+  ticketModalInfo (ticket) {
+    var ticketInfo = [];
 
-const editTicket = function () {
+    for (var key in ticket) {
+      ticketInfo.push([key, ticket[key]]);
+    }
 
-};
+    return ticketInfo;
+  };
 
-const deleteTicket = function () {
+  // componentDidMount () {
+  //   this.props.loadTicketInfoToState(this.props.ticket);
+  // }
 
-};
+  editTicket () {
 
-const TicketModal = ({ticket}) => (
-    <td>
-      <a href={`#openModal${ticket._id}`}>More Info</a>
+  };
 
-      <div id={`openModal${ticket._id}`} className="modalDialog">
-        <div>
-          <a href="#closeModal" title="Close" className="closeModal">X</a>
-          <h2>Ticket Details</h2>
-          <p>
-            {ticketModalInfo(ticket).map(ticketInfo => (
+  deleteTicket () {
+
+  };
+
+  render () {
+    return (
+      <td>
+        <a href={`#openModal${this.props.ticket._id}`}>More Info</a>
+
+        <div id={`openModal${this.props.ticket._id}`} className="modalDialog">
+          <div>
+            <a href="#closeModal" title="Close" className="closeModal">X</a>
+            <h2>Ticket Details</h2>
+            <p>
+            {this.ticketModalInfo(this.props.ticket).map(ticketInfo => (
               <span id={`preview${ticketInfo[0]}`}>{ticketInfo.join(': ')}<br /></span>
-            ))}
-          </p>
-          <button className='btn btn-default' data-toggle='collapse' data-target={`#editTicket${ticket._id}`}>Edit Ticket</button>
-          <button className='btn btn-default' name='delete'>Delete Ticket</button>
+            ))
+            }
+            </p>
+            <button className='btn btn-default' onClick={() => (alert(JSON.stringify(this.props.ticket)))} data-toggle='collapse' data-target={`#editTicket${this.props.ticket._id}`}>Edit Ticket</button>
+            <button className='btn btn-default' name='delete'>Delete Ticket</button>
 
-          <div className='collapse' id={`editTicket${ticket._id}`}>
-            <EditTicketForm className='collapse' />
+            <div className='collapse' id={`editTicket${this.props.ticket._id}`}>
+              <EditTicketForm ticket={this.props.ticket} className='collapse' />
+            </div>
           </div>
         </div>
-      </div>
-    </td>
-);
+      </td>
+    );
+  }
+
+}
 
 export default TicketModal;
