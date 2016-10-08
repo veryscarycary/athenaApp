91279177ad7920d4c64a884386c83af091e1d072
Bradford Melluish <bradmelluish@gmail.com>
2016-10-08 09:28:46 -0700

    Implements Postgres Database Connection Utilities
    KB up and running. More coming soon!

--- client/components/Nav/Nav.js
+++ client/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { browserHistory } from 'react-router';
-import beefalo from '../images/beefalo.png';
+import beefalo from '../../images/beefalo.png';
 import DashboardButton from './DashboardButton';
 
 import sessionUtils from '../../utils/sessionUtils';
