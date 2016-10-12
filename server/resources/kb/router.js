733a78cbca0296cac0921b6bd4aacd3a46e8de31
Bradford Melluish <bradmelluish@gmail.com>
2016-10-12 12:03:55 -0700

    Implement Router Updates

--- server/resources/kb/router.js
+++ server/resources/kb/router.js
@@ -2,19 +2,16 @@
 const router = require('../../config/middleware.js').router();
 const api = require('./controller.js');
 
-// returns stub list based on search params from search service. also used to get individual stubs.
-// router.get('/api/kb/search', api.searchKb); TODO: PENDING IMPLEMENTATION OF SEARCH SERVICE
-
-router.route('/api/kb/articles')
-  .post(api.getArticles)//gets articles for ticketModal
+router.route('/api/kbMetrics')
+  .get(api.getArticleRelations);
 
 router.route('/api/kb')
-  .get(api.getKb) //gets all KBs
-  .post(api.createKb);//creates a KB
+  .get(api.getArticle) //gets all KBs
+  .post(api.createArticle);//creates a KB
 
 router.route('/api/kb/:id')
-  .get(api.getKb) //returns a KB
-  .put(api.editKb)//edits a KB
-  .delete(api.deleteKb);//deletes a KB
+  .get(api.getArticle) //returns a KB
+  .put(api.editArticle)//edits a KB
+  .delete(api.deleteArticle);//deletes a KB
 
 module.exports = router;
