9fbf5aed84dc2bda479d47e2cbf18d5e2b2009d5
Cary <carymeskell@gmail.com>
2016-10-14 14:50:37 -0700

    Fix not-found page

--- client/components/NotFoundComponent.js
+++ client/components/NotFoundComponent.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const NotFoundComponent = () => (
-  <div>
-    <div className='loginTop'>
-      <div className='loginTopTrim'>
+  <div className='login-container'>
+    <div className='login'>
+      <div className='loginSignupContainer'>
+      <div>
+        <h1 className='title'>NOT FOUND</h1><br />
+        <p className='subtitle'>Sorry. You are either not authorized to view this page or the page does not exist.</p>
+        <div className='button-right-float'>
+          <Link to='/'><button className='read-more'>Home</button></Link>
+        </div>
       </div>
-    </div>
-    <div className='centerFlex'>
-      <h1>NOT FOUND</h1><br />
-      <p>Sorry. You are either not authorized to view this page or the page does not exist.</p>
+      </div>
     </div>
   </div>
 );
