c4ec79a01b380d267f18f290cea55025e06ce5df
Cary Meskell <carymeskell@gmail.com>
2024-10-20 00:57:02 -0700

    fix urls while using docker compose

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,7 +1,7 @@
 module.exports = {
   default: 3000,
-  user: 'http://localhost:3001',
-  ticket: 'http://localhost:3002',
-  kb: 'http://localhost:3003',
-  kbSearch: 'http://localhost:3004',
+  user: `http://${process.env.DOCKER_COMPOSE ? 'user' : 'localhost'}:3001`,
+  ticket: `http://${process.env.DOCKER_COMPOSE ? 'ticket' : 'localhost'}:3002`,
+  kb: `http://${process.env.DOCKER_COMPOSE ? 'kb' : 'localhost'}:3003`,
+  kbSearch: `http://${process.env.DOCKER_COMPOSE ? 'kb-search' : 'localhost'}:3004`,
 };
